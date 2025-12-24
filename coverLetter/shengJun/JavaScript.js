9ab3d29b6cfdc61dfba20f5ab2d8ebb3fac9fa3f
Darrenc0901 <darrenchuashengjun@gmail.com>
2025-12-24 13:56:32 +0800

    JavaScript.js

---
+++ coverLetter/shengJun/JavaScript.js
@@ -0,0 +1,8 @@
+function toggleSkills() {
+  var Skills = document.getElementById("Skills");
+  if (skills.style.display == "none") {
+      skills.style.display = "block";
+  } else {
+      skills.style.display = "none";
+  }
+}
